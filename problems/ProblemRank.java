29908c05111d9348bc8e5bca102edafd4e9256d9
신관규 <73925242+Soundbar91@users.noreply.github.com>
2023-11-14 20:20:18 +0900

    필드 수정

--- problems/ProblemRank.java
+++ problems/ProblemRank.java
@@ -8,16 +8,26 @@ import java.io.Serializable;
 public class ProblemRank implements Serializable {
 	
 	private int ID;
-	private User user;
+	private String user;
 	private RANK Rank;
 	private String Comment;
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Override
+	public String toString() {
+	    return "ProblemRank{" +
+	            "ID=" + ID +
+	            ", user=" + user +
+	            ", Rank=" + Rank +
+	            ", Comment='" + Comment + '\'' +
+	            '}';
+	}
+	
 	public ProblemRank() {}
 	public ProblemRank(int ID, User user, RANK Rank, String Comment) {
 		this.ID = ID;
-		this.user = user;
+		this.user = user.getUsername();
 		this.Rank = Rank;
 		this.Comment = Comment;
 	}
@@ -31,7 +41,7 @@ public class ProblemRank implements Serializable {
 	public int getID(){
 		return this.ID;
 	}
-	public User getuser() {
+	public String getuser() {
 		return this.user;
 	}
 	
