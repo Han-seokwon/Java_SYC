30fd7961341ae7b5d7d6525c8fa4412f6cd32925
seokwon <hoh91700001@gmail.com>
2023-11-19 08:04:17 +0900

    ProblemTable url 열에 폰트 적용

--- JavaApp/src/gui/ProblemTable.java
+++ JavaApp/src/gui/ProblemTable.java
@@ -1,6 +1,10 @@
 package gui;
 
+import java.awt.Color;
+import java.awt.Component;
 import java.awt.Desktop;
+import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.IOException;
@@ -8,37 +12,109 @@ import java.net.URI;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Vector;
 
+import javax.swing.JLabel;
 import javax.swing.JTable;
-import javax.swing.SwingUtilities;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
 
 import problems.Problem;
 import users.User;
 
+
+//테이블 헤더 정보를 저장하는 열거형
+enum TABLE_HEADER{ 
+	ID(0, "문제 번호", 100), TITLE(1, "문제 제목", 300), RANK(2, "난이도", 100), URL(3, "URL", 300);
+
+	private int idx; // 헤더 인덱스
+	private String name; // 헤더 이름
+	private int width; // 헤더 너비
+	private TABLE_HEADER(int idx, String name, int width) {
+		this.idx = idx;
+		this.name = name;
+		this.width = width;
+
+	}
+	public int getIdx() {
+		return idx;
+	}
+	public String getName() {
+		return name;
+	}
+	public int getWidth() {
+		return width;
+	}
+	// 헤더요소(열거형)의 개수를 반환 
+	public static int length() {
+		return TABLE_HEADER.values().length;
+	}
+	// 헤더요소(열거형)의 문자열 이름을 벡터로 반환, DefaultTableModel에서 컬럼이름(헤더)를 Vector로 받음
+	public static Vector<String> getHeaderNames() {
+		Vector<String> headerNames = new Vector<>(TABLE_HEADER.length());
+		for(TABLE_HEADER header : TABLE_HEADER.values()) {
+			headerNames.add(header.getName());
+		}
+		return headerNames;    	
+	}
+}
+
+// 문제리스트를 동적으로 할당받아 테이블을 구성하는 클래스
 public class ProblemTable extends JTable{
 	private static final long serialVersionUID = 1L;
-	private final static int ROW_HEIGHT = 30;  // 행 높이
+	private final static int ROW_HEIGHT = 40;  // 행 높이
 	// 테이블에 동적으로 데이터를 추가하기 위한 모델 객체 생성
 	private DefaultTableModel tableModel;
 	private ArrayList<Problem> recommendedProblemList = new ArrayList<>(); // 추천된 문제 리스트
 	private User user;// 현재 로그인된 유저
-	
+
 	public ProblemTable(User user) {
 		super();// JTable 멤버 상속받기
 		this.user = user; // 현재 로그인된 유저데이터 가져오기
-		
+
 		// 테이블에 동적으로 데이터를 추가하기 위한 테이블 모델 객체 생성
 		tableModel = new DefaultTableModel(TABLE_HEADER.getHeaderNames(), 0) { // 테이블 헤더, row 개수
 			private static final long serialVersionUID = 1L;
 			@Override
 			public boolean isCellEditable(int row, int column) { return true; } // 셀 내용 수정할 수 없게 오버라이딩
 		};
-		setModel(tableModel); // 테이블 모델 적용		
+		setModel(tableModel); // 테이블 모델 적용
+
+		// 헤더 높이 설정
+		Dimension headerSize = getTableHeader().getPreferredSize();
+		headerSize.height = ROW_HEIGHT;
+		getTableHeader().setPreferredSize(headerSize);
+		// 테이블 높이, 너비, 폰트 설정		
+		for(TABLE_HEADER header : TABLE_HEADER.values()) {
+			TableColumn col = getColumnModel().getColumn(header.getIdx());
+			col.setPreferredWidth(header.getWidth()); // 너비 설정
+			col.setCellRenderer(new TableCellRenderer()); // 지정한 텍스트 폰트 적용해주는 TableCellRenderer 생성 후 적용
+		}		
+		
 		setRowHeight(ROW_HEIGHT); // 행 높이 설정
+
+
 		addMouseListener(new tableClickListener()); // 테이블 클릭에 대한 이벤트 리스너 등록
 	}
-	
+
+	// 지정한 텍스트 폰트 적용해주는 CellRenderer 생성 
+	class TableCellRenderer extends DefaultTableCellRenderer{
+		public TableCellRenderer() {
+			super();
+			setHorizontalAlignment(JLabel.CENTER);
+		}
+		@Override
+		public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+			Component component = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);              
+			if(column == TABLE_HEADER.URL.getIdx()) { // url 열
+				setText("<html><u>" + value + "</u></html>");// 밑줄 적용
+	            setForeground(Color.BLUE);// 파란색 적용
+			}
+			return this;
+		}
+	}
+
 	class tableClickListener extends MouseAdapter{ // 테이블 클릭에 대한 이벤트 리스너		
 		public void mouseClicked(MouseEvent e) {
 			int row = getSelectedRow();
@@ -54,17 +130,15 @@ public class ProblemTable extends JTable{
 				// 해당 열에 맞는 문제 정보 조회 프레임 생성
 				Problem selectedProblem = recommendedProblemList.get(row);
 				System.out.println("문제 선택됨\n" + selectedProblem);
-//				new ProblemViewerFrame(selectedProblem, user); // 클래스 완성시 코드 추가
+				//				new ProblemViewerFrame(selectedProblem, user); // 클래스 완성시 코드 추가
 			}
 		}
 	}
-	
-	public void clearTableRows() {
+
+
+	//  문제리스트로 테이블 업데이트
+	public void updateProblemListToTable( List<Problem> recommendedProblemList) throws ClassCastException{
 		tableModel.setNumRows(0); // 기존에 입력된 테이블 행 초기화
-	}
-	
-	// 추천된 문제리스트를 테이블에 추가
-	public void addProblemListToTable( List<Problem> recommendedProblemList) throws ClassCastException{
 		this.recommendedProblemList = new ArrayList<>(recommendedProblemList);
 		Iterator<Problem> it = recommendedProblemList.iterator();
 		String[] tableRowValues = new String[TABLE_HEADER.length()];
@@ -75,11 +149,11 @@ public class ProblemTable extends JTable{
 			tableRowValues[TABLE_HEADER.TITLE.getIdx()] = problem.getProblemName();
 			tableRowValues[TABLE_HEADER.RANK.getIdx()] = problem.getProblemRank().getRankName();
 			tableRowValues[TABLE_HEADER.URL.getIdx()] = problem.getProblemURL();	
-	        tableModel.addRow(tableRowValues); // row 데이터 테이블에 추가
+			tableModel.addRow(tableRowValues); // row 데이터 테이블에 추가
 		}
 	}
-	
-	
+
+
 }
 
 
--- JavaApp/src/gui/RecommendProblemFrame.java
+++ JavaApp/src/gui/RecommendProblemFrame.java
@@ -1,16 +1,16 @@
 package gui;
 
 import java.awt.Color;
-import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Vector;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -20,42 +20,10 @@ import javax.swing.SwingConstants;
 import javax.swing.border.CompoundBorder;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
-import javax.swing.table.DefaultTableModel;
 
 import problems.Problem;
 import problems.ProblemDBManager;
 import users.User;
-
-
-// 테이블 헤더 정보를 저장하는 열거형
-enum TABLE_HEADER{ 
-	ID(0, "문제 번호"), TITLE(1, "문제 제목"), RANK(2, "난이도"), URL(3, "URL");
-
-	private int idx; // 헤더 인덱스
-	private String name; // 헤더 이름
-	private TABLE_HEADER(int idx, String name) {
-		this.idx = idx;
-		this.name = name;
-	}
-	public int getIdx() {
-		return idx;
-	}
-	public String getName() {
-		return name;
-	}
-	// 헤더요소(열거형)의 개수를 반환 
-	public static int length() {
-		return TABLE_HEADER.values().length;
-	}
-	// 헤더요소(열거형)의 문자열 이름을 벡터로 반환, DefaultTableModel에서 컬럼이름(헤더)를 Vector로 받음
-	public static Vector<String> getHeaderNames() {
-		Vector<String> headerNames = new Vector<>(TABLE_HEADER.length());
-		for(TABLE_HEADER header : TABLE_HEADER.values()) {
-			headerNames.add(header.getName());
-		}
-		return headerNames;    	
-	}
-}
 
 
 public class RecommendProblemFrame extends JFrame {
@@ -64,14 +32,8 @@ public class RecommendProblemFrame extends JFrame {
 	private JPanel contentPane;
 	private JButton recommendByRankButton, recommendByAlgorithmTypeButton ; // 난이도별 추천 버튼, 알고리즘별 추천 버튼
 	private JLabel seletedBtnDisplayLabel; 	// 선택된 추천방식 버튼 결과를 보여주는 라벨
-	private ProblemTable recommendedProblemTable;//추천 문제 테이블
-	// 테이블에 동적으로 데이터를 추가하기 위한 모델 객체 생성
-	private DefaultTableModel tableModel = new DefaultTableModel(TABLE_HEADER.getHeaderNames(), 0) { // 테이블 헤더, row 개수
-		@Override
-		public boolean isCellEditable(int row, int column) { return true; } // 셀 내용 수정할 수 없게 오버라이딩
-	};  
+	private ProblemTable recommendedProblemTable;// 추천된 문제를 담는 테이블
 	private User user;// 현재 로그인된 유저
-	private ArrayList<Problem> recommendedProblemList = new ArrayList<>(); // 추천된 문제 리스트
 
 	public RecommendProblemFrame(User user) {	
 		this.user= user; // 현재 로그인된 유저
@@ -85,7 +47,7 @@ public class RecommendProblemFrame extends JFrame {
 		
 		// contentPane 레이아웃 설정
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setBorder(new EmptyBorder(20, 5, 5, 5));
 		setContentPane(contentPane);
 		GridBagLayout gbl_contentPane = new GridBagLayout();
 		gbl_contentPane.columnWidths = new int[]{FRAME_WIDTH};
@@ -95,10 +57,13 @@ public class RecommendProblemFrame extends JFrame {
 
 		// 문제 추천 메뉴 레이아웃 설정
 		JPanel recommendMenuPanel = new JPanel();
-		contentPane.add(recommendMenuPanel);
+		GridBagConstraints gbc_recommendMenuPanel = new GridBagConstraints();
+		gbc_recommendMenuPanel.fill = GridBagConstraints.BOTH;
+		contentPane.add(recommendMenuPanel, gbc_recommendMenuPanel);
 		GridBagLayout gbl_recommendMenuPanel = new GridBagLayout();
-		gbl_recommendMenuPanel.columnWidths = new int[] {300, 300, 300};
-		gbl_recommendMenuPanel.rowHeights = new int[] {150};
+		final int MENU_PANEL_WIDTH = 300;
+		gbl_recommendMenuPanel.columnWidths = new int[] {MENU_PANEL_WIDTH, MENU_PANEL_WIDTH, MENU_PANEL_WIDTH};
+		gbl_recommendMenuPanel.rowHeights = new int[] {MENU_PANEL_WIDTH/2};
 		recommendMenuPanel.setLayout(gbl_recommendMenuPanel);
 
 		// 추천방식 선택 버튼용 패널 추가
@@ -135,7 +100,11 @@ public class RecommendProblemFrame extends JFrame {
 		recommendMethodButtonPanel.add(recommendByRankButton, gbc_recommendByRankButton);
 
 		// 화살표 이미지 라벨 추가
-		JLabel arrowImgLabel = new JLabel("->"); // 나중에 이미지로 대체
+		ImageIcon arrowImg = new ImageIcon(RecommendProblemFrame.class.getResource("/images/right-arrow.png"));
+		int imgWidth = (int)(MENU_PANEL_WIDTH*2/3);
+		Image resizedImg = arrowImg.getImage().getScaledInstance(imgWidth, imgWidth/2, Image.SCALE_SMOOTH); // 사이즈 조정		
+		JLabel arrowImgLabel = new JLabel(new ImageIcon(resizedImg)); 
+		arrowImgLabel.setBounds(0, 0, 150, 150);
 		GridBagConstraints gbc_arrowImgLabel = new GridBagConstraints();
 		gbc_arrowImgLabel.gridx = 1;
 		gbc_arrowImgLabel.gridy = 0;
@@ -153,6 +122,7 @@ public class RecommendProblemFrame extends JFrame {
 
 		// 추천된 문제 리스트를 담는 패널 레이아웃 설정
 		JPanel recommendedProblemListPanel = new JPanel();
+		recommendedProblemListPanel.setBorder(createCompoundBorder(5, 20)); // 경계선 설정
 		GridBagConstraints gbc_recommendedProblemListPanel = new GridBagConstraints();
 		gbc_recommendedProblemListPanel.gridx = 0;
 		gbc_recommendedProblemListPanel.gridy = 1;
@@ -178,7 +148,7 @@ public class RecommendProblemFrame extends JFrame {
 		gbc_scrollPane.gridy = 1;
 		recommendedProblemListPanel.add(scrollPane, gbc_scrollPane);
 
-		recommendedProblemTable = new ProblemTable(user); // 테이블에 테이블 모델 설정
+		recommendedProblemTable = new ProblemTable(user); // 추천된 문제들을 담는 테이블 생성
 		scrollPane.setViewportView(recommendedProblemTable);
 
 		setVisible(true); // 윈도우 show
@@ -193,7 +163,6 @@ public class RecommendProblemFrame extends JFrame {
 	class ButtonActionListener implements ActionListener{
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			recommendedProblemTable.clearTableRows(); // 기존에 입력된 테이블 행 초기화
 			List<Problem> recommendedProblemList = new ArrayList<>();
 
 			JButton selectedButton = (JButton)e.getSource(); 
@@ -207,12 +176,12 @@ public class RecommendProblemFrame extends JFrame {
 			} else if(selectedButton == recommendByRankButton) {				
 				System.out.println("난이도별 추천 버튼 clicked!");
 				// 테스트용 -> 추후 삭제
-				recommendedProblemList = ProblemDBManager.findProblemSearch("함수");
+				recommendedProblemList = ProblemDBManager.findProblemSearch("수");
 				System.out.println(recommendedProblemList);				
 				// recommendedProblemList = RecommendProblem.메서드이름(user) // 클래스 완성시 추가
 			}
 			seletedBtnDisplayLabel.setText(selectedButton.getText()); // 클릭된 버튼에 맞게 라벨 내용을 변경
-			recommendedProblemTable.addProblemListToTable(recommendedProblemList); // 추천된 문제리스트를 테이블에 추가
+			recommendedProblemTable.updateProblemListToTable(recommendedProblemList); // 추천된 문제리스트를 테이블에 추가
 
 		}
 	}
